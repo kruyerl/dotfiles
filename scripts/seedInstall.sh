78fd4078b725932a46343b17d22c4cd2a21cb27f
Luke Kruyer <kruyerl@gmail.com>
2024-10-13 18:30:04 -0400

    Split gitconfig into seperate item

--- scripts/seedInstall.sh
+++ scripts/seedInstall.sh
@@ -1,29 +1,21 @@
 echo "Installing"
 
 sudo apt update && sudo apt upgrade -y
-sudo apt install -y software-properties-common build-essentials
-sudo apt install -y git curl wget tmux unzip  
+sudo apt install -y software-properties-common build-essential
+sudo apt install -y git curl wget tmux unzip 
 sudo apt autoremove && sudo apt autoclean
-
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage
-sudo mv nvim.appimage /usr/local/bin/nvim
 
 # Configure Git
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git config --global user.email "kruyerl@gmail.com"
-git congig --global user.name "kruyerl"
-ssh-keygen -t ed25519 -C "kruyerl@gmail.com"
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_ed25519
-cat ~/.ssh/id_ed25519.pub
-echo "Copy the above key to https://github.com/settings/keys "
-read -p "push enter to continue"
-read gitcontinue
 
-#set up node
+# Install Nvim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+sudo mv nvim.appimage /usr/local/bin/nvim
+
+# Install Fast Node Manager
 curl -fsSL https://fnm.vercel.app/install | bash --skip-shell
 
 source ~/.zshrc
