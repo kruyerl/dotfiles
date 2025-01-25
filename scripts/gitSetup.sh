80df9268c2e2b2af37bd9d9960d906f16ed9aa9a
kruyerl <kruyerl@gmail.com>
2025-01-24 22:50:50 -0500

    Added ranger to init

--- scripts/gitSetup.sh
+++ scripts/gitSetup.sh
@@ -1,5 +1,6 @@
 sudo apt install git
-git config --global user.email "kruyerl@gmail.com" && git config --global user.name "kruyerl"
+git config --global user.email "kruyerl@gmail.com"
+git config --global user.name "kruyerl"
 
 git remote set-url origin git@github.com:kruyerl/dotfiles.git
 
